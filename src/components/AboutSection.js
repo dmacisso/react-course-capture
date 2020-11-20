781c3f761f747a9af58e6fc8e6ffec0991be9384
dmacisso <dmacisso@gmail.com>
2020-11-20 12:18:43 -0500

    About Section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import home1 from '../img/home1.png';
+
+const AboutSection = () => {
+  return (
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>We work to make</h2>
+          </div>
+          <div className="hide">
+            <h2>
+              your <span>dreams</span>
+            </h2>
+          </div>
+          <div className="hide">
+            <h2>come true.</h2>
+          </div>
+        </div>
+        <p>
+          Contact our skilled professionals for any photography or videography
+          ideas that you have
+        </p>
+        <button>Contact Us</button>
+      </div>
+      <div className="image">
+        <img src={home1} alt="Photographer with Camera" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
